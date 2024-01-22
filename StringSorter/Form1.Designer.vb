f89c74d7e5749c3c3c553e370df9d9695c2a4f77
HA施彥宇 <yenyu@ym.local>
2024-01-22 10:40:36 +0800

    測試第一次

---
+++ StringSorter/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(396, 64)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(35, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(355, 107)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(35, 125)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(355, 31)
+        Me.TextBox2.TabIndex = 2
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(35, 182)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(355, 208)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(508, 423)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ StringSorter/Form1.vb
@@ -0,0 +1,166 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        ' 從 TextBox 取得原始字串
+        Dim originalString As String = TextBox1.Text
+
+        ' 新增的字串
+        Dim newString As String = TextBox2.Text
+
+        ' 將原始字串轉為字串陣列
+        Dim stringsToCheck As String() = originalString.Split("|"c)
+
+        ' 檢查順序是否正確
+        If Not IsOrderCorrect(stringsToCheck) Then
+            ' 順序不正確，顯示錯誤信息並標註紅色
+            DisplayOrderError(originalString, stringsToCheck)
+            Return
+        End If
+
+        ' 順序正確，新增字串到列表中，保持字母順序
+        Dim resultArray As String() = InsertIntoSortedArray(stringsToCheck, newString)
+
+        ' 將結果輸出到 RichTextBox，不改變顏色
+        DisplayInRichTextBox(resultArray)
+    End Sub
+
+    Function IsOrderCorrect(ByVal strings As String()) As Boolean
+        ' 檢查字串陣列是否按照字母順序排列
+        For i As Integer = 1 To strings.Length - 1
+            If String.Compare(strings(i - 1), strings(i), StringComparison.Ordinal) > 0 Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+
+    Sub DisplayOrderError(ByVal originalString As String, ByVal stringsToCheck As String())
+        ' 顯示錯誤信息，標註紅色
+        RichTextBox1.Clear()
+
+        ' 第一行輸出原本的字串代碼
+        RichTextBox1.AppendText(originalString)
+
+        ' 換行
+        RichTextBox1.AppendText(vbCrLf)
+
+        ' 第二行輸出應該修改的順序，並將有修改順序的字串以紅色顏色標註
+        For i As Integer = 1 To stringsToCheck.Length - 1
+            If String.Compare(stringsToCheck(i - 1), stringsToCheck(i), StringComparison.Ordinal) > 0 Then
+                RichTextBox1.SelectionColor = Color.Red
+            Else
+                RichTextBox1.SelectionColor = RichTextBox1.ForeColor
+            End If
+
+            RichTextBox1.AppendText(stringsToCheck(i - 1))
+            RichTextBox1.AppendText("|")
+        Next
+
+        ' 最後一個字串
+        RichTextBox1.AppendText(stringsToCheck(stringsToCheck.Length - 1))
+    End Sub
+
+    Function InsertIntoSortedArray(ByVal strings As String(), ByVal newString As String) As String()
+        ' 新增字串到有序字串陣列中，保持字母順序
+        Dim index As Integer = 0
+
+        While index < strings.Length AndAlso String.Compare(newString, strings(index), StringComparison.Ordinal) > 0
+            index += 1
+        End While
+
+        Array.Resize(strings, strings.Length + 1)
+
+        For i As Integer = strings.Length - 1 To index + 1 Step -1
+            strings(i) = strings(i - 1)
+        Next
+
+        strings(index) = newString
+
+        Return strings
+    End Function
+
+    Sub DisplayInRichTextBox(ByVal strings As String())
+        ' 顯示文字在 RichTextBox 中，改變新增字串的顏色為紅色
+        RichTextBox1.Clear()
+
+        For i As Integer = 0 To strings.Length - 1
+            If i > 0 Then
+                RichTextBox1.AppendText("|")
+            End If
+
+            ' 檢查是否為新增的字串，如果是，則設定紅色
+            If strings(i) = TextBox2.Text Then
+                RichTextBox1.SelectionColor = Color.Red
+            Else
+                RichTextBox1.SelectionColor = RichTextBox1.ForeColor
+            End If
+
+            RichTextBox1.AppendText(strings(i))
+        Next
+    End Sub
+End Class
+
+
+
+
+
+
+
+'Public Class Form1
+
+'    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+'        ' 從 TextBox 取得原始字串
+'        Dim originalString As String = TextBox1.Text
+
+'        ' 新增的字串
+'        Dim newString As String = TextBox2.Text
+
+'        ' 將原始字串轉為字串陣列
+'        Dim stringsToCheck As String() = originalString.Split("|"c)
+
+'        ' 新增字串到列表中，保持字母順序
+'        Dim resultArray As String() = InsertIntoSortedArray(stringsToCheck, newString)
+
+'        ' 將結果輸出到 RichTextBox，不改變顏色
+'        DisplayInRichTextBox(resultArray)
+'    End Sub
+
+'    Function InsertIntoSortedArray(ByVal strings As String(), ByVal newString As String) As String()
+'        ' 新增字串到有序字串陣列中，保持字母順序
+'        Dim index As Integer = 0
+
+'        While index < strings.Length AndAlso String.Compare(newString, strings(index), StringComparison.Ordinal) > 0
+'            index += 1
+'        End While
+
+'        Array.Resize(strings, strings.Length + 1)
+
+'        For i As Integer = strings.Length - 1 To index + 1 Step -1
+'            strings(i) = strings(i - 1)
+'        Next
+
+'        strings(index) = newString
+
+'        Return strings
+'    End Function
+
+'    Sub DisplayInRichTextBox(ByVal strings As String())
+'        ' 顯示文字在 RichTextBox 中，改變新增字串的顏色為紅色
+'        RichTextBox1.Clear()
+
+'        For i As Integer = 0 To strings.Length - 1
+'            If i > 0 Then
+'                RichTextBox1.AppendText("|")
+'            End If
+
+'            ' 檢查是否為新增的字串，如果是，則設定紅色
+'            If strings(i) = TextBox2.Text Then
+'                RichTextBox1.SelectionColor = Color.Red
+'            Else
+'                RichTextBox1.SelectionColor = RichTextBox1.ForeColor
+'            End If
+
+'            RichTextBox1.AppendText(strings(i))
+'        Next
+'    End Sub
+
+'End Class
